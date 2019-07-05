7fb1960b0c2fe5b4be9a53e9f550ce508f906637
mkusaka <hinoshita1992@gmail.com>
2019-07-06 01:24:01 +0900

    refactoring

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/mkusaka/parser_example
 
 go 1.12
 
-require github.com/mkusaka/sitemapparser v0.0.0-20190701142758-7bf18c91ff6e
+require (
+	github.com/mkusaka/sitemapparser v0.0.0-20190701142758-7bf18c91ff6e
+	github.com/motemen/go-loghttp v0.0.0-20170804080138-974ac5ceac27
+	github.com/motemen/go-nuts v0.0.0-20180315145558-42c35bdb11c2 // indirect
+)
--- main.go
+++ main.go
@@ -1,25 +1,69 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"strconv"
 	"strings"
 
 	"github.com/mkusaka/sitemapparser"
+	_ "github.com/motemen/go-loghttp/global"
 )
 
 func main() {
-	file, err := os.Create(`output.csv`)
+	if len(os.Args) <= 1 {
+		log.Fatal(`argument requierd
+1st argument: index sitemap url
+2nd argument: replace from collected site url string
+3nd argument: replace to collected site url string`)
+		return
+	}
+	indexSitemapURL := os.Args[1]
+
+	replaceFromString := ""
+	if len(os.Args) >= 3 {
+		replaceFromString = os.Args[2]
+	}
+
+	replaceToString := ""
+	if len(os.Args) >= 4 {
+		replaceToString = os.Args[3]
+	}
+
+	file, err := os.Create("output.csv")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
-	output, err := sitemapparser.Scheduler("some good gzipped sitemap url")
+	targetURLs, err := sitemapparser.Scheduler(indexSitemapURL)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	file.Write(([]byte)(strings.Join(output, "\n")))
+	statusResutls := []string{"url, statusCode"}
+
+	for i, url := range targetURLs {
+		if i >= 2 {
+			break
+		}
+		targetURL := strings.Replace(url, replaceFromString, replaceToString, 1)
+		resp, err := http.Get(targetURL)
+
+		status := ""
+		if err != nil {
+			fmt.Println(err)
+			status = err.Error()
+		} else {
+			status = strconv.Itoa(resp.StatusCode)
+			resp.Body.Close()
+		}
+
+		statusResutls = append(statusResutls, targetURL+","+status)
+	}
+
+	file.Write(([]byte)(strings.Join(statusResutls, "\n")))
 }
